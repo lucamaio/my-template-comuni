8b6eb26b124ede3ff0251f5fb5a71e85da933e37
lucamaio <lucos.maio@gmail.com>
2025-06-03 09:15:19 +0200

    beta Single elemento trasparenza

---
+++ single-elemento_trasparenza.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ *  elemento_trasparenza template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Design_Comuni_Italia
+ */
+
+global $uo_id, $inline, $audio;
+
+get_header();
+?>
+
+    <main>
+        <?php 
+        while ( have_posts() ) :
+            the_post();
+            $user_can_view_post = dci_members_can_user_view_post(get_current_user_id(), $post->ID);
+
+            $prefix= '_dci_elemento_trasparenza_';
+            $descrizione_breve = dci_get_meta("descrizione_breve", $prefix, $post->ID);
+            $file = dci_get_meta("file", $prefix, $post->ID);
+            var_dump($file);
+            ?>
+            <div class="container" id="main-container">
+                <div class="row">
+                    <div class="col px-lg-4">
+                        <?php get_template_part("template-parts/common/breadcrumb"); ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 px-lg-4 py-lg-2">
+                    <?php if (preg_match('/[A-Z]{5,}/', get_the_title())) {
+                                   echo '<h1 data-audio>'.ucfirst(strtolower(get_the_title())).'</h1>';
+                                }else{
+                                    echo '<h1 data-audio>'. get_the_title().'</h1>';
+                                }
+                        ?>
+                        <h2 class="visually-hidden" data-audio>Dettagli della notizia</h2>
+                        <?php if (preg_match('/[A-Z]{5,}/', $descrizione_breve)) {
+                                   echo '<p data-audio>'.ucfirst(strtolower($descrizione_breve)).'</p>';
+                                }else{
+                                    echo '<p data-audio>'. $descrizione_breve.'</p>';
+                                }
+                        ?>     
+                    </div>
+                    <div class="col-lg-3 offset-lg-1">
+                        <?php
+                        $inline = true;
+                        get_template_part('template-parts/single/actions');
+                        ?>
+                    </div>                 
+                </div>                      
+            </div>
+
+            <div class="container">
+                <div class="row border-top border-light row-column-border row-column-menu-left">
+                    <aside class="col-lg-4">
+                        <div class="cmp-navscroll sticky-top" aria-labelledby="accordion-title-one">
+                            <nav class="navbar it-navscroll-wrapper navbar-expand-lg" aria-label="Indice della pagina" data-bs-navscroll>
+                                <div class="navbar-custom" id="navbarNavProgress">
+                                    <div class="menu-wrapper">
+                                        <div class="link-list-wrapper">
+                                            <div class="accordion">
+                                                <div class="accordion-item">
+                                                    <span class="accordion-header" id="accordion-title-one">
+                                                        <button
+                                                            class="accordion-button pb-10 px-3 text-uppercase"
+                                                            type="button"
+                                                            aria-controls="collapse-one"
+                                                            aria-expanded="true"
+                                                            data-bs-toggle="collapse"
+                                                            data-bs-target="#collapse-one"
+                                                        >INDICE DELLA PAGINA
+                                                            <svg class="icon icon-sm icon-primary align-top">
+                                                                <use xlink:href="#it-expand"></use>
+                                                            </svg>
+                                                        </button>
+                                                    </span>
+                                                    <div class="progress">
+                                                        <div class="progress-bar it-navscroll-progressbar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                    <div id="collapse-one" class="accordion-collapse collapse show" role="region" aria-labelledby="accordion-title-one">
+                                                        <div class="accordion-body">
+                                                            <ul class="link-list" data-element="page-index">
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" href="#descrizione">
+                                                                    <span class="title-medium">Descrizione</span>
+                                                                    </a>
+                                                                </li>
+                                                                <?php if( is_array($documenti) && count($documenti) ) { ?>
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" href="#documenti">
+                                                                    <span class="title-medium">Documenti</span>
+                                                                    </a>
+                                                                </li>
+                                                                <?php } ?>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </aside>
+                    <section class="col-lg-8 it-page-sections-container border-light">
+                    <?php get_template_part('template-parts/single/image-large'); ?>
+                    <article class="it-page-section anchor-offset" data-audio>                        
+                        <h4 id="descrizione">Descrizione</h4>
+                        <div class="richtext-wrapper lora">
+                            <?php if (preg_match('/[A-Z]{5,}/', $descrizione_breve)) {
+                                   echo ucfirst(strtolower($descrizione_breve));
+                                }else{
+                                    echo $descrizione_breve;
+                                }
+                            ?>
+                        </div>
+                    </article>   
+                    <?php if( is_array($file) && count($file) ) { ?>
+                    <article class="it-page-section anchor-offset mt-5">
+                        <h4 id="documenti">Documenti</h4>
+                        <div class="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal">
+                            <?php foreach ($file as $doc_id) {
+                                $documento = get_post($doc_id);
+                            ?>
+                            <div class="card card-teaser shadow-sm p-4 mt-3 rounded border border-light flex-nowrap">
+                                <svg class="icon" aria-hidden="true">
+                                <use
+                                    xlink:href="#it-clip"
+                                ></use>
+                                </svg>
+                                <div class="card-body">
+                                <h5 class="card-title">
+                                    <a class="text-decoration-none" href="<?php echo get_permalink($doc_id); ?>" aria-label="Visualizza il documento <?php echo $documento->post_title; ?>" title="Visualizza il documento <?php echo $documento->post_title; ?>">
+                                        <?php echo $documento->post_title; ?>
+                                    </a>
+                                </h5>
+                                </div>
+                            </div>
+                            <?php } ?>
+                        </div>
+                    </article>
+                    <?php } ?>
+                        </div>
+                      </div>
+                </div>
+            </div>
+
+            <?php get_template_part("template-parts/common/valuta-servizio"); ?>
+
+
+        <?php
+        endwhile; // End of the loop.
+        ?>
+
+<?php
+get_footer();?>

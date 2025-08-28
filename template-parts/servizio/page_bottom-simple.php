6a8b38edc9cd1c0cdcfe6fc8585f8b6400f2e533
lucamaio <lucos.maio@gmail.com>
2025-08-28 16:42:52 +0200

    Remove deprecated template parts related to services and contact points, including more-posts, page bottom sections, people listings, contact points, reviews, related content, external services, and all services. This cleanup improves maintainability and reduces unused code.

--- template-parts/servizio/page_bottom-simple.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    global $post;
-
-    // metadata
-    $last_edit = explode(' ', $post->post_modified);
-    $last_edit_day = implode('/',array_reverse(explode('-',$last_edit[0])));
-?>
-
-<p class="text-paragraph-small mb-0">Pagina aggiornata il <?php echo $last_edit_day; ?></p>

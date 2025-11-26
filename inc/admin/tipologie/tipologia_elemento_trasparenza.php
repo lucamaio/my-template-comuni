65e3ce442b121b843084ea1c618028d34c4ece5d
Luca Maio <lucos.maio@gmail.com>
2025-11-26 08:40:40 +0100

    Update 20/11
    Added support for .rar and .7zip file formats in the file upload input.

--- inc/admin/tipologie/tipologia_elemento_trasparenza.php
+++ inc/admin/tipologie/tipologia_elemento_trasparenza.php
@@ -191,7 +191,7 @@ function dci_render_transparency_multipost_page() {
                     <tr>
                         <th scope="row"><label for="dci_multi_files"><?php _e('Carica Documenti Multipli:', 'design_comuni_italia'); ?></label></th>
                         <td>
-                            <input type="file" id="dci_multi_files" name="dci_multi_files[]" multiple accept=".pdf,.doc,.docx,.xls,.xlsx,.zip">
+                            <input type="file" id="dci_multi_files" name="dci_multi_files[]" multiple accept=".pdf,.doc,.docx,.xls,.xlsx,.zip,.rar,.7zip">
                             <p class="description"><?php _e('Seleziona più documenti da caricare. Verrà creato un Elemento Trasparenza per ogni file.', 'design_comuni_italia'); ?></p>
                         </td>
                     </tr>

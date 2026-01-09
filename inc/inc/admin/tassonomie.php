0efc588dfbb84e73e2f3ad820f09c7cc096051a5
lucamaio <lucos.maio@gmail.com>
2026-01-09 10:06:08 +0100

    Semove cartella inc duplicata

--- inc/inc/admin/tassonomie.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//include tutti i file che descrivono le tassonomie del Sito dei Comuni
-foreach(glob(get_template_directory() . "/inc/admin/tassonomie/*.php") as $file){
-    require $file;
-}
